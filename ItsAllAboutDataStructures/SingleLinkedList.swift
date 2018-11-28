1f0aca50b06d6556c60269a8a11e5a0aace40d73
Saket Bhushan <v-saket.bhushan@yatra.com>
2018-11-28 08:48:15 +0530

    linked list insertion at index

--- ItsAllAboutDataStructures/SingleLinkedList.swift
+++ ItsAllAboutDataStructures/SingleLinkedList.swift
@@ -46,10 +46,87 @@ class SingleLinkedList<T> {
             }
             temp?.next = element
         }
-        
-        
-        
     }
     
     
 }
+/*
+ //: Playground - noun: a place where people can play
+ 
+ import UIKit
+ 
+ class Node<T> {
+ 
+ var data: T?
+ var next: Node<T>?
+ 
+ init(data: T) {
+ self.data = data
+ }
+ }
+ 
+ class SingleLinkedList<T> {
+ 
+ var head: Node<T>?
+ 
+ var isEmpty: Bool {
+ return head == nil
+ }
+ 
+ var first: Node<T>? {
+ return head
+ }
+ 
+ // MARK : Appending
+ func append(value: T) {
+ let element = Node<T>(data: value)
+ if self.isEmpty {
+ self.head = element
+ } else {
+ if var temp = head {
+ while temp.next != nil {
+ temp = temp.next!
+ }
+ temp.next = element
+ }
+ }
+ }
+ 
+ func insertAt(value: T, index: Int) {
+ let element = Node<T>(data: value)
+ var temp = self.head
+ for i in 0 ..< index where temp?.next != nil {
+ temp = temp?.next
+ }
+ element.next = temp?.next
+ temp?.next = element
+ }
+ 
+ func printList() {
+ var output = "["
+ var temp = self.head
+ while (temp != nil) {
+ output += "\(temp?.data as? String ?? ""  )" + ","
+ temp = temp?.next
+ }
+ print(output + "]")
+ }
+ 
+ }
+ 
+ 
+ var sll = SingleLinkedList<String>()
+ sll.append(value: "saket1")
+ sll.append(value: "saket2")
+ sll.append(value: "saket3")
+ sll.append(value: "saket4")
+ sll.insertAt(value: "insertSaket", index: 8)
+ sll.printList()
+ 
+ 
+ 
+ 
+ 
+
+ 
+ */
